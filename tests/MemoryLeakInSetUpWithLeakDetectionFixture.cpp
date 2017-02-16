396a0b013e5a5164f698ee49dfeace0494bbf5a3
Michał Wypych <mwypych@agh.edu.pl>
2017-02-16 13:30:59 +0100

    Adds disabled test checking memory leakage in test fixture tear down/setup

--- tests/MemoryLeakInSetUpWithLeakDetectionFixture.cpp
+++ tests/MemoryLeakInSetUpWithLeakDetectionFixture.cpp
@@ -1,5 +1,5 @@
 //
-// Created by mwypych on 15.02.17.
+// Created by mwypych on 16.02.17.
 //
 
 #include <gtest/gtest.h>
@@ -8,21 +8,22 @@
 #include <MemorySpy.h>
 #include "LeakDetectionTest.h"
 
-class LeakDetectionWithLeakDetectionFixture : public virtual LeakDetectionTest {
+class MemoryLeakInSetUpWithLeakDetectionFixture : public virtual LeakDetectionTest {
 
  public:
   std::string *testing_string;
  protected:
   void SetUp() override {
+    std::cout<<__PRETTY_FUNCTION__<<" running"<<std::endl;
     ::testing::Test::SetUp();
     testing_string = new std::string();
   }
   void TearDown() override {
     ::testing::Test::TearDown();
-    delete testing_string;
+    //delete testing_string;
   }
 };
 
-TEST_F(LeakDetectionWithLeakDetectionFixture,Create) {
+TEST_F(MemoryLeakInSetUpWithLeakDetectionFixture,DISABLED_Create) {
   ASSERT_TRUE(true);
 }
